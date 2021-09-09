a64afeb0e28fb802f6ee7ff628a7d1572bcc62d5
Alsu Shaykhidinova <Shaykhidinova@avp.ru>
2021-09-10 00:15:48 +0300

    Исправление ошибок

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,8 +14,8 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert assert self.is_element_present(*MainPageLocators.REG_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Register form is not presented"
